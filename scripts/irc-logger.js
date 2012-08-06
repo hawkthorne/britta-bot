83e711b9454b9668ca85f575e12607b172bcda2e
Jordan Hoff <jhoff@linkedin.com>
2012-08-06 01:20:50 -0500

    Rebuild of irc logger

--- scripts/irc-logger.js
+++ scripts/irc-logger.js
@@ -1,5 +1,5 @@
 //Description:
-//  Logs events to redis
+//  Logs irc events to redis
 //
 //Dependencies:
 //  None
@@ -14,44 +14,119 @@
 //  jhoff
 
 module.exports = function(robot) {
-	robot.enter( function(msg) {
-		if( msg.message.user.name != robot.name ) {
-			write_message( 'join', msg );
-		}
-	});
-	robot.leave( function(msg) {
-		if( msg.message.user.name != robot.name ) {
-			write_message( 'part', msg );
-		}
-	});
-	robot.catchAll( function(msg) {
-		write_message( 'text', msg );
-	});
-	robot.adapter.bot.on('nick',function( oldnick, newnick, channels ) {
-		for( var i in channels ) {
-			write_log(
-				channels[i],
-				'nick',
-				{
-					from: oldnick,
-					to: newnick
-				}
-			);
-		}
-	});
 
-	function write_message( type, msg ) {
-		_room = msg.message.user.room;
-		_msg = { user: msg.message.user.name };
-		if( type == 'text' ) {
-			_msg.text = msg.message.text;
+	// listen for all events that the irc bot emits
+	robot.adapter.bot
+		.on( 'kick',		function() { emit_listener( 'kick',			arguments ); } )
+		.on( 'message',		function() { emit_listener( 'message',		arguments ); } )
+		.on( 'error',		function() { emit_listener( 'error',		arguments ); } )
+		.on( 'join',		function() { emit_listener( 'join',			arguments ); } )
+		.on( 'part',		function() { emit_listener( 'part',			arguments ); } )
+		.on( 'quit',		function() { emit_listener( 'quit',			arguments ); } )
+		.on( 'nick',		function() { emit_listener( 'nick',			arguments ); } )
+		.on( 'selfMessage',	function() { emit_listener( 'selfMessage',	arguments ); } );
+
+	function emit_listener( type, args ) {
+		switch( type ) {
+			case 'kick':
+				// channel, who, by, reason
+				write_log(
+					args[0],
+					type,
+					{
+						user: args[1],
+						by: args[2],
+						reason: args[3]
+					}
+				);
+				break;
+			case 'message':
+				// from, channel, text
+				write_log(
+					args[1],
+					type,
+					{
+						user: args[0],
+						message: args[2]
+					}
+				);
+				break;
+			case 'selfMessage':
+				// to, text
+				if( args[0].indexOf('#') == 0 ) {
+					write_log(
+						args[0],
+						'message',
+						{
+							from: robot.name,
+							message: args[1]
+						}
+					);
+				}
+				break;
+			case 'error':
+				// message
+				write_log(
+					'errors',
+					type,
+					{
+						message: args[0]
+					}
+				);
+				break;
+			case 'join':
+				// channel, who
+				write_log(
+					args[0],
+					type,
+					{
+						user: args[1]
+					}
+				);
+				break;
+			case 'part':
+				// channel, who, reason
+				write_log(
+					args[0],
+					type,
+					{
+						user: args[1],
+						reason: args[2]
+					}
+				);
+				break;
+			case 'quit':
+				// who, reason, channels
+				for( var i in args[2] ) {
+					write_log(
+						args[2][i],
+						type,
+						{
+							user: args[0],
+							reason: args[1]
+						}
+					);
+				}
+				break;
+			case 'nick':
+				// old nick, new nick, channels
+				for( var i in args[2] ) {
+					write_log(
+						args[2][i],
+						type,
+						{
+							from: args[0],
+							to: args[1]
+						}
+					);
+				}
+				break;
 		}
-		write_log( _room, type, _msg );
 	}
 
 	function write_log( room, type, data ) {
 		room = room.trim();
-		console.log('pushing', data, 'to logs_' + room);
+		console.log(type + ': pushing', data, 'to logs_' + room);
 		robot.redisclient.lpush( 'logs_' + room, JSON.stringify( {
 			type: type,
 			stamp: Math.floor((new Date()).getTime() / 1000),
